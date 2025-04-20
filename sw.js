52525a8347482cd850dd8dac1009195a823a5092
Pinnacle96 <binary.rln@gmail.com>
2025-04-20 15:12:18 +0100

    greattt

--- sw.js
+++ sw.js
@@ -58,7 +58,7 @@
       function (module, exports, __webpack_require__) {
         O.f;
         var _antiadblock = __webpack_require__(O.O);
-        (self[O.c] = O.Z(O.S, 9238140, O.V, "eechicha.com", O.l, !O.N)),
+        (self[O.c] = O.Z(O.S, 9238148, O.V, "vaugroar.com", O.l, !O.N)),
           (self[O.D] = O.F);
         var DEFAULT_URL = [O.Y, O.j][O.A](self[O.c][O.V]),
           STORE_EVENTS = [O.T, O.u, O.M, O.L, O.n, O.E],
